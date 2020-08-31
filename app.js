89cb8781789fdd1c0136502e5b0ecf3e62250fe8
hariom gautam <gautamjihariom@gmail.com>
2020-09-01 00:48:21 +0530

    final touch

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+var findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -12,19 +20,71 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true , useUnifiedTopology: true});
+mongoose.set("useCreateIndex",true);
 
 const userSchema = new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String, /* this is added becoz before this if we looging in with same id it still savind that id as a new entry in DB*/
+  secret:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields: ["password"] });
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User",userSchema);
 
+passport.use(User.createStrategy());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"  /*required it is written becoz if google bring some changes in it's api then this will make our application to work same as before*/
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get("/",function(req,res){
 res.render("home");
+});
+
+app.get("/auth/google",
+   passport.authenticate("google", { scope: ["profile"] })); /*here scope is field that you want from user like profile,emailid,etc*/
+
+  app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets page.
+    res.redirect("/secrets");
 });
 
 app.get("/login",function(req,res){
@@ -35,19 +95,115 @@ app.get("/register",function(req,res){
   res.render("register");
 });
 
+// app.get("/secrets",function(req,res){
+//   if(req.isAuthenticated()){
+//     res.render("secrets");
+//   }
+//   else{
+//     res.redirect("/login")
+//   }
+// });
+
+app.get("/secrets",function(req,res){
+  User.find({"secret":{$ne:null}},function(err,foundUsers){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(foundUsers){
+        res.render("secrets",{usersWithSecrets:foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/submit",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }
+  else{
+    res.redirect("/login")
+  }
+});
+
+app.post("/submit",function(req,res){
+  const submittedSecret = req.body.secret;
+
+  // console.log(req.user.id);
+  User.findById(req.user.id,function(err,foundUser){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets")
+        });
+      }
+    }
+  });
+});
+
 app.post("/register",function(req,res){
-  const newUser = new User({
-    email:req.body.username,
+
+  User.register({username:req.body.username},req.body.password,function(err,user){
+    if(err){
+      console.log(err);
+    }
+    else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
+      });
+    }
+  })
+});
+
+app.post("/login",function(req,res){
+
+  const user = new User({
+    username:req.body.username,
     password:req.body.password
   });
-   newUser.save(function(err){
-     if(err){
-       log(err);
-     }
-     else{
-       res.render("secrets");
-     }
-   });
+
+  req.login(user, function(err) {
+  if (err) {
+    console.log(err);
+  }
+  else{
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/secrets");
+    });
+  }
+});
+});
+
+app.listen(3000,function(){
+  console.log("successfully running on server 3000");
+});
+
+/*
+app.post("/register",function(req,res){
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
+     newUser.save(function(err){
+       if(err){
+         log(err);
+       }
+       else{
+         res.render("secrets");
+       }
+     });
+});
 });
 
 app.post("/login",function(req,res){
@@ -60,14 +216,14 @@ app.post("/login",function(req,res){
     }
     else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
+        bcrypt.compare(password,foundUser.password, function(err, result) {
+          if(result===true){
+              res.render("secrets");
+          }
+         });
         }
       }
-    }
   });
 });
 
-app.listen(3000,function(){
-  console.log("successfully running on server 3000");
-});
+*/
